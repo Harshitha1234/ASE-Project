d2937bb7bd9d06754703a44c229229eca9b58519
Harshitha1234 <harshithachowdary.t17@iiits.in>
2018-11-18 17:41:30 +0530

    delivery main P

--- Customers/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='Cus_index'),
-    path('signup/', views.signup, name='Cus_signup'),
-    path('categories/', views.categories, name='Cus_categories'),
-]
--- Main/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from . import templates
-from django.http import HttpResponse
-
-def index(request):
-    return render(request, 'Main/index.html')
-
-
